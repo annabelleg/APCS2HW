e14dc101b3da137942c6622944b703824aaea3e3
annabelleg <agary110@gmail.com>
2015-02-05 22:10:11 -0500

    initial code

---
+++ 01recursion/Recursion.java
@@ -0,0 +1,12 @@
+public class Recursion implements hw1{
+
+    public String name(){
+	return "Gary,Annabelle";
+    }
+
+    public int fact(int n){return 0;}
+    public int fib(int n){return 0;}
+
+    public double sqrt(double n){return 0.0;}
+
+}
