c92891b15b5e069dd1c74b3c8d84bf1c61dd63b7
annabelleg <agary110@gmail.com>
2015-02-26 21:19:36 -0500

    it workssss

--- 03NQueens/NQueens.java
+++ 03NQueens/NQueens.java
@@ -20,50 +20,71 @@ public class NQueens{
 	return "gary.annabelle";
     }
     public boolean solve(){
-	return solve(0,0,0);
+	return solve(0,0,1);
     }
     public boolean solve(int x){
-	return solve(0,x,0);
+	return solve(x,0,1);
     }
     public boolean solve(int x, int y, int num){
 	if (x >= N || y >= N || x < 0 || y < 0){
 	    return false;
 	}
-	if (num >= N){
-	    board[x][y] = num;
-	    return true;
-	}
-	if (num < N){
-	    //vertical
-	    for (int r = 0; r < N; r++){
-		if (board[r][y] != 0 && r != x) return false;
-	    }
-	    //horizontal
-	    for (int c = 0; c < N; c++){
-		if (board[x][c] != 0 && c != y) return false;
-	    }
-	    //diagonal
-	    for (int d = 0; d < N; d++){
-		//	if (board[x+d]
-		
-	    }
+	if (board[x][y] != 0){
 	    return false;
+	}
+	for (int i = 0; i < N; i++){
+	    //vertical
+	    if (board[i][y] != 0 && i != x) return false;
+	    //horizontal
+	    if (board[x][i] != 0 && i != y) return false;
+	    //diagonal
+	    if (y + i < board.length && x + i < board.length){
+		if (board[x+i][y+i] != 0) return false;
+	    }
+	    if (y + i < board.length && x - i >= 0){
+		if (board[x-i][y+i] != 0) return false;
+	    }
+	    if (y - i  >= 0 && x + i < board.length){
+		if (board[x+i][y-i] != 0) return false;
+	    }
+	    if (y - i >= 0 && x - i >= 0){
+		if (board[x-i][y-i] != 0) return false;
+	    }
 	}
 	if (board[x][y] == 0){
 	    board[x][y] = num;
-	    if (solve(x+1, 0, num+1) ||
-		solve(x+1, y, num+1) ||
-		solve(x, y+1, num+1) ||
-		solve(x+1, y+1, num+1)){
-		return true;
+	    for (int i = 0; i < N; i++){
+		if (solve(i, y+1, num + 1)) return true;
 	    }
 	    board[x][y] = 0;
 	}
+	if (num == N){
+	    board[x][y] = num;
+	    return true;
+	}
 	return false;
     }
+
     public static void main(String[]args){
-	NQueens n = new NQueens(4);
-	n.solve();
-	System.out.println(n);
+	NQueens a = new NQueens(5);
+	System.out.println(a.name());
+	System.out.println("NQueens:");
+    
+	NQueens b = new NQueens(5);
+	b.solve();
+	System.out.println(b);
+
+	NQueens c = new NQueens(5);
+	c.solve(3);
+	System.out.println(c);
+
+	NQueens d = new NQueens(6);
+	if(d.solve()){
+	    System.out.println(d);
+	}else{
+	    System.out.println("Cannot solve");
+	}
+
+	System.out.println("Hi Mr. K!!!");
     }
 }
