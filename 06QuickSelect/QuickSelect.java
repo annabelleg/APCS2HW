257fdf42565e69a1ed7acc175cf85728cb29f111
annabelleg <agary110@gmail.com>
2015-03-05 23:16:16 -0500

    Update QuickSelect.java

--- 06QuickSelect/QuickSelect.java
+++ 06QuickSelect/QuickSelect.java
@@ -1,9 +1,6 @@
 public class QuickSelect{
-    static int[] ary;
-    public QuickSelect(int[] a){
-	ary = a;
-    }
-    public static void partition( int si, int ei){
+
+    public static void partition(int[]ary, int si, int ei){
 	int[] D = new int[ary.length];
 	for (int i = 0; i < si; i++){
 	    D[i] = ary[i];
@@ -25,23 +22,20 @@ public class QuickSelect{
 	    }
 	
 	}
-	D[si] =pivot;// ary[si];
+	D[si] =pivot;
         for (int x = 0; x < ary.length; x++){
 	    ary[x] = D[x];
 	}
     }
-    public String toString(){
-	String r = "[";
-	for (int i = 0; i < ary.length-1; i++){
-	    r+=ary[i] + ", ";
-	}
-        r+=ary[ary.length-1] + "] \n";
-	return r;
-    }
+
     public static void main(String[]args){
 	int[]a = {5,6,20000,534,4,967665765,11,1,8,-1};
-	QuickSelect b = new QuickSelect(a);
-        b.partition(0,9);
-	System.out.println(b);
+        partition(a,0,9);
+	System.out.print("[");
+	for (int i = 0; i < a.length-1; i++){
+	    System.out.print(a[i] + ", ");
+	}
+	System.out.println(a[a.length-1] + "] \n");
+	
     }
 }
