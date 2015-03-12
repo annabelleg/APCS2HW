0f354ef78e7597fd2cf860e895bf488974f31072
annabelleg <agary110@gmail.com>
2015-03-11 22:06:14 -0400

    gottems

--- 07LNode/MyLinkedList.java
+++ 07LNode/MyLinkedList.java
@@ -26,16 +26,100 @@ public class MyLinkedList{
 	current.setNext(next);
 
     }
-    /*  public int get(int index);
-    public void set(int index,int value);
-    public void add(int value, int index);
-    public int remove(int index);
-    public int size();
-    public int indexOf(int value);*/
+    public void add(int index, int value){
+	if (index < 0 || index > this.size()){
+	    throw new ArrayIndexOutOfBoundsException();
+	}
+	if (index == this.size()){
+	    this.add(value);
+	    return;
+	}
+	current = head;
+	while (index > 0){
+	    current = current.getNext();
+	    index--;
+	}
+	//get val and next node of place you're shifting down
+	int temp = current.getValue();
+	//set val of index
+	current.setValue(value);
+	//shift values down
+	current.setNext(new LNode(temp, current.getNext()));
+    }
+    public int size(){
+	int s = 0;
+	current = head;
+	while (current.getNext() != null){
+	    s++;
+	    current = current.getNext();
+	}
+	return s+1;
+    }
+    public int get(int index){
+	current = head;
+	while (index > 0){
+	    index--;
+	    current = current.getNext();
+	}
+	return current.getValue();
+	
+    }
+    public void set(int index, int value){
+	current = head;
+	while (index > 0){
+	    index--;
+	    current = current.getNext();
+	}
+	current.setValue(value);
+    }
+    public int remove(int index){
+	current = head;
+	while (index > 0){
+	    index--;
+	    current = current.getNext();
+	}
+	int val = current.getValue();
+	LNode next = current.getNext();
+	while(next.getNext()!=null){
+	    current.setValue(next.getValue());
+	    current = next;
+	    next = next.getNext();
+	}
+	current.setValue(next.getValue());
+	current.setNext(null);
+	return val;
+    }
+    
+    public int indexOf(int value){
+	if (head.getValue()==value) return 0;
+	current = head;
+	int counter = 0;
+	while (current.getNext()!=null){
+	    current = current.getNext();
+	    counter++;
+	    if (current.getValue()==value){
+		return counter;
+	    }
+	}
+	return -1;
+    }
+
+
     public static void main(String[]args){
 	MyLinkedList l = new MyLinkedList(3);
 	l.add(2);
+	l.add(7);
+	l.add(4);
+	l.add(10);
+	l.add(-1);
+	l.add(7);
+	l.add(4, 10000);
+	l.set(4,-999999);
+	System.out.println(l.remove(4));
 	System.out.println(l);
+	System.out.println(l.size());
+	System.out.println(l.get(0));
+	System.out.println(l.indexOf(5));
     }
 }
 
