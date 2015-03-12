78843bec87e4ba6fb51fb17ca0e1fcc88ea4deb5
annabelleg <agary110@gmail.com>
2015-03-11 22:37:46 -0400

    Update LNode.java

--- 07LNode/LNode.java
+++ 07LNode/LNode.java
@@ -2,6 +2,16 @@ public class LNode{
     
     private int value;
     private LNode next;
+    
+    public LNode(){
+    }
+    public LNode(int v){
+	value = v;
+    }
+    public LNode(int v, LNode n){
+	value = v;
+	next = n;
+    }
 
     public LNode getNext(){
 	return next;
@@ -18,5 +28,7 @@ public class LNode{
     public String toString(){
 	return ""+value;
     }
+
+
 
 }
