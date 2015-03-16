6a7ae5e1a7abe64b4bdb70e03d7766e01f1a1c49
annabelleg <agary110@gmail.com>
2015-03-16 13:01:00 -0400

    generics yo

--- 07LNode/LNode.java
+++ 07LNode/LNode.java
@@ -1,34 +1,31 @@
-public class LNode{
+public class LNode<T>{
     
-    private int value;
-    private LNode next;
+    private T value;
+    private LNode<T> next;
     
     public LNode(){
     }
-    public LNode(int v){
+    public LNode(T v){
 	value = v;
     }
-    public LNode(int v, LNode n){
+    public LNode(T v, LNode<T> n){
 	value = v;
 	next = n;
     }
 
-    public LNode getNext(){
+    public LNode<T> getNext(){
 	return next;
     }
-    public int getValue(){
+    public T getValue(){
 	return value;
     }
-    public void setNext(LNode l){
+    public void setNext(LNode<T> l){
 	next = l;
     }
-    public void setValue(int n){
+    public void setValue(T n){
 	value = n;
     }
     public String toString(){
 	return ""+value;
     }
-
-
-
 }
