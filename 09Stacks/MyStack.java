bd3f2da76f30357d77c9219bbe989628383aad76
annabelleg <agary110@gmail.com>
2015-03-22 18:59:27 -0400

    woot

--- 09Stacks/MyStack.java
+++ 09Stacks/MyStack.java
@@ -1,5 +1,7 @@
-public class MyStack{
-   LinkedList<T> stack;
+import java.util.*;
+
+public class MyStack<T>{
+    LinkedList<T> stack;
     
     public MyStack(){
 	stack = new LinkedList<T>();
@@ -14,5 +16,19 @@ public class MyStack{
 	stack.add(item);
 	return item;
     }
-
+    public String toString(){
+	return stack.toString();
+    }
+    public static void main(String[]args){
+	MyStack<Integer> a = new MyStack<Integer>();
+	a.push(5);
+	a.push(7);
+	System.out.println(a);
+	System.out.println(a.peek());
+	System.out.println(a.pop());
+	System.out.println(a.pop());
+	System.out.println(a.pop());
+	System.out.println(a);
+	
+    }
 }
