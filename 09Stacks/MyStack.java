2d34d9b51ebc5184c52ab683d11858cd5e25dae7
annabelleg <agary110@gmail.com>
2015-03-22 18:51:29 -0400

    added methods

--- 09Stacks/MyStack.java
+++ 09Stacks/MyStack.java
@@ -1,7 +1,18 @@
 public class MyStack{
- LinkedList<T> stack;
+   LinkedList<T> stack;
     
     public MyStack(){
 	stack = new LinkedList<T>();
     }
+    public T pop(){
+	return stack.removeLast();
+    }
+    public T peek(){
+	return stack.peekLast();
+    }
+    public T push(T item){
+	stack.add(item);
+	return item;
+    }
+a
 }
