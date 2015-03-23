cf10a37cfec650eb565ff33d7c80185046231839
annabelleg <agary110@gmail.com>
2015-03-23 13:15:34 -0400

    note: remove doesnt work

--- 09Stacks/MyStack.java
+++ 09Stacks/MyStack.java
@@ -1,34 +1,29 @@
-import java.util.*;
-
 public class MyStack<T>{
-    LinkedList<T> stack;
-    
+    private MyLinkedList<T> stack;
+
     public MyStack(){
-	stack = new LinkedList<T>();
-    }
-    public T pop(){
-	return stack.removeLast();
-    }
-    public T peek(){
-	return stack.peekLast();
+	stack = new MyLinkedList<T>();
     }
     public T push(T item){
 	stack.add(item);
 	return item;
+    }
+    public T peek(){
+	return stack.tail.getValue();
+    }
+    public T pop(){
+	return stack.remove(stack.size()-1);
     }
     public String toString(){
 	return stack.toString();
     }
     public static void main(String[]args){
 	MyStack<Integer> a = new MyStack<Integer>();
-	a.push(5);
-	a.push(7);
-	System.out.println(a);
-	System.out.println(a.peek());
-	System.out.println(a.pop());
-	System.out.println(a.pop());
+	a.push(2);
+	a.push(4);
+	a.push(6);
+	a.push(8);
 	System.out.println(a.pop());
 	System.out.println(a);
-	
     }
 }
