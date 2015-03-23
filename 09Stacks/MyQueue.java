77a5544f7abb0b0c21c6d5f0664b3c7b45762fac
annabelleg <agary110@gmail.com>
2015-03-23 13:15:00 -0400

    Create MyQueue.java

---
+++ 09Stacks/MyQueue.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class MyQueue<T>{
+    private MyLinkedList<T> queue;
+    public MyQueue(){
+	queue = new MyLinkedList<T>();
+    }
+    public boolean enqueue(T val){
+	queue.add(val);
+	return true;
+    }
+    public T dequeue(){
+	if (queue.size()==0){
+	    throw new NoSuchElementException();
+	}
+	return queue.remove(0);
+    }
+    public String toString(){
+	return queue.toString();
+    }
+    public static void main(String[]args){
+	MyQueue<Integer> a = new MyQueue<Integer>();
+	a.enqueue(2);
+	a.enqueue(3);
+	a.enqueue(4);
+	a.enqueue(5);
+	System.out.println(a);
+	a.dequeue();
+	a.dequeue();
+	a.dequeue();
+	a.dequeue();
+	//	a.dequeue();
+	System.out.println(a);
+    }
+}
