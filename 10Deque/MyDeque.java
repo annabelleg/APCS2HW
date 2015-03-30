a2785636b33dd17cfc919d08d493d957a4a02e85
annabelleg <agary110@gmail.com>
2015-03-30 00:22:51 -0400

    added toString

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -2,6 +2,12 @@ public class MyDeque<T>{
     Object[] data;
     int head, tail;
 
+    public MyDeque(){
+	data = new Object[10];
+	head = 0;
+	tail = head;
+    }
+
     public void addFirst(T value){
 	if (head+1 != tail && tail+1 != head){
 	    if (head != 0 && tail != data.length-1){
@@ -67,6 +73,32 @@ public class MyDeque<T>{
 	    i++;
 	}
 	return larger;
+    }
+    public String toString(){
+	if (data.length == 0){
+	    return "[ ]";
+	}
+	String r = "[ ";
+	if (head < tail){
+	    for (int i = head; i < tail; i++){
+		r+=data[i] + " ";
+	    }
+	}else{
+	    for (int h = head; h < data.length; h++){
+		r+=data[h] + " ";
+	    }
+	    for (int t = 0; t <= tail; t++){
+		r+=data[t] + " ";
+	    }
+	}
+	r+= "] \nhead = "+ head + "\ntail = " + tail;
+	return r;
+    }
+
+    public static void main(String[]args){
+	MyDeque<Integer> d = new MyDeque<Integer>();
+	//	addFirst(2);
+	System.out.println(d);
     }
 
 }
