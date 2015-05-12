748e1fb573ba889bb5cbfb191949fff32abefe35
annabelleg <agary110@gmail.com>
2015-05-11 20:59:02 -0400

    Update MyHeap.java

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,29 +1,55 @@
 import java.util.*;
 public class MyHeap{
     private ArrayList<Integer> data;
+    int size;
     
 
     public MyHeap(){
-	data = new ArrayList<Integer>(1);
-	data.set(0, 0);
+	data = new ArrayList<Integer>();
+	size = 0;
+        data.add(size);
     } 
     public String toString(){
 	String result = "";
 	for (int i = 1; i < data.size(); i*=2){
 	    for (int j = i; j < 2*i; j++){
-		result += data.get(j);
+		result += data.get(j) + " ";
 	    }
+	    result += "\n";
 	}
 	return result;
     }
 
     public int remove(){
-	return data.get(0);
+	return data.get(1);
     } 
     public void add(int n){
-	if (data.size() == 1){
+	if (data == null){
 	    data.add(n);
-	    data.set(0, 1);
+	    size++;
+	    data.set(0,size);
+	    return;
 	}
+	if (data.get(0) == 0){
+	    data.add(n);
+	    size++;
+	    data.set(0, size);
+	    return;
+	} else {
+	    //   int temp = data.get(0) + 1;
+	    // data.set(0, temp);
+	    return;
+	}
+    }
+    public int getSize(){
+	return size;
+    }
+
+    public static void main(String[]args){
+	MyHeap h = new MyHeap();
+	h.add(4);
+	//	System.out.println(h.remove());
+	System.out.println(h);
+	System.out.println("Size: " + h.getSize());
     }
 }
