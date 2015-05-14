1da384ab8b0eab1751747663daf0dc632df355dd
annabelleg <agary110@gmail.com>
2015-05-14 13:17:33 -0400

    Update RunningMedian.java

--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -2,19 +2,23 @@ public RunningMedian{
     
     private MyHeap large;
     private MyHeap small;
-    private int median;
+    private double median;
 
     public RunningMedian(){
 	large = new MyHeap(false);
 	small = new MyHeap(true);
     }
 
-    public int getMedian(){
+    public double getMedian(){
 	return calculateMedian();
     }
-    public int calculateMedian(){
-	median = ( large.remove() + small.remove() )/2;
+    public double calculateMedian(){
+	median = ( large.remove() + small.remove() )/2.0;
 	return median;
+    }
+    public void add(int value){
+	if (value > double) large.add(value);
+	if (value < double) small.add(value);
     }
     
 }
