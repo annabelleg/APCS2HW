01fa5d7092fe5f9c62aa4867f144bca09655ea30
annabelleg <agary110@gmail.com>
2015-05-18 19:49:55 -0400

    Update RunningMedian.java

--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -1,4 +1,4 @@
-public RunningMedian{
+public class RunningMedian{
     
     private MyHeap large;
     private MyHeap small;
@@ -9,16 +9,58 @@ public RunningMedian{
 	small = new MyHeap(true);
     }
 
+    public String name(){
+	return "gary.annabelle";
+    }
+
+
     public double getMedian(){
 	return calculateMedian();
     }
     public double calculateMedian(){
-	median = ( large.remove() + small.remove() )/2.0;
-	return median;
+	if (small.getSize() == 0 && large.getSize() == 0){
+	    return 0.0;
+	}
+	if (small.getSize() > large.getSize()){
+	    return small.peek() + 0.0;
+	}
+	if (large.getSize() > small.getSize()){
+	    return large.peek() + 0.0;
+	}
+	return ((small.peek() + large.peek())/2.0);
+	
     }
     public void add(int value){
-	if (value > double) large.add(value);
-	if (value < double) small.add(value);
+	if (value > median) 
+	    large.add(value);
+	else
+	    small.add(value);
+	balance();
     }
-    
+    public void balance(){
+	int l = large.getSize();
+	int s = small.getSize();
+	if (Math.abs(l - s) > 1){
+	    if (l > s){
+		small.add(large.remove());
+	    }else{
+		large.add(small.remove());
+	    }
+	}
+    }
+    public static void main(String[]args){
+	RunningMedian r = new RunningMedian();
+	System.out.println(r.name());
+	System.out.println(r.getMedian());
+	r.add(5);
+	System.out.println(r.getMedian());
+	r.add(12);
+	System.out.println(r.getMedian());
+	r.add(0);
+	System.out.println(r.getMedian());
+	r.add(3);
+	System.out.println(r.getMedian());
+	r.add(4);
+	System.out.println(r.getMedian());
+    }
 }
