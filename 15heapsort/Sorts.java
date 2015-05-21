a5e4c40686e6dc4f568b82b8a5af14f76a24d927
annabelleg <agary110@gmail.com>
2015-05-20 23:14:58 -0400

    doesnt work yet but i think its close!

--- 15heapsort/Sorts.java
+++ 15heapsort/Sorts.java
@@ -2,10 +2,10 @@ import java.util.Arrays;
 
 public class Sorts{
 
-   public static String name(){
-    return "gary.annabelle";
-  }
-  
+    public static String name(){
+	return "gary.annabelle";
+    }
+
     public static String toString(int[] ary){
 	String ans = "[ ";
 	for (int i = 0; i < ary.length-1; i++){
@@ -18,7 +18,7 @@ public class Sorts{
 
     public static int[] heapsort(int[] ary){
         ary = heapify(ary);
-	return ary;
+	return heapsort(ary, ary.length-1);
     }
 
 
@@ -45,7 +45,41 @@ public class Sorts{
 	}
 	return ary;
     }
-  
+    
+    public static int[] heapsort(int[] ary, int end){
+	if (end != 1){
+	    int temp = ary[end];
+	    ary[end] = ary[0];
+	    ary[0] = temp;
+	    pushDownIntoPlace(ary, 0);
+	    return heapsort(ary, end-1);
+	}
+	return ary;
+	
+    }
+    
+    public static void pushDownIntoPlace(int[]ary, int index){
+	if (ary.length == 3){
+	    if (ary[index] < ary[index+1]){
+		int temp = ary[index];
+		ary[index] =  ary[index+1];
+		ary[index+1] =  temp;
+	    }
+	}
+	else if (index*2+1 < ary.length){               
+	    int larger = 2*index;
+	    if (ary[larger+1] > ary[larger]){
+		larger ++;
+		}
+	    if (ary[index] < ary[larger]){
+		int temp = ary[index];
+		ary[index] =  ary[larger];
+		ary[larger]= temp;
+		pushDownIntoPlace(ary, larger);
+	    } else { return; }
+	    
+	}
+    }
     public static void main(String[]args){
 	int[] a = new int[10];
 	for (int i = 0; i < 10; i++){
@@ -53,6 +87,7 @@ public class Sorts{
 	}
 	int[] ary = {4,1,0,6,3,8};
 	//	Sorts.heapsort(ary);
+	//	System.out.println(name());
 	System.out.println(Sorts.toString(Sorts.heapsort(a)));
     }
    
